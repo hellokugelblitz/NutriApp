7d7597fd5f4a34d9ba4f746d5110ff93f63c0a5a
thatnoobles <danildonchuk@gmail.com>
2023-10-16 21:40:33 -0400

    new directory structure now includes unit tests :)

--- src/main/workout/WorkoutController.cs
+++
@@ -1,9 +0,0 @@
-namespace NutriApp.Workout;
-
-using System.Collections.Generic;
-
-public class WorkoutController {
-    public List<Workout> GenerateRecommendedWorkouts() {
-        return null;
-    }
-}
