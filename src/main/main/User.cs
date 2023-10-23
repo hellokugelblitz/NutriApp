538210484b55149f01adfc8857821929ecb77f80
crazyforMcCafe <rayhmiot@gmail.com>
2023-10-23 01:03:29 -0400

    begun implementation of user persistence

--- src/main/main/User.cs
+++ src/main/main/User.cs
@@ -1,12 +1,16 @@
 using System;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace NutriApp;
 
 public class User {
-    private string name;
-    private int height; //in inches
-    private double weight;
-    private DateTime birthday;
+    private readonly string userPath = $"{Persistence.UserDataPath}\\user.json";
+
+    [JsonProperty] private string name;
+    [JsonProperty] private int height; //in inches
+    [JsonProperty] private double weight;
+    [JsonProperty] private DateTime birthday;
 
     public User(string name, int height, double weight, DateTime birthday) {
         this.name = name;
@@ -19,4 +23,20 @@ public class User {
     public int GetHeight{get => height;}
     public double GetWeight{get => weight;}
     public DateTime GetBirthday{get => birthday;}
+
+    public void Save()
+    {
+        // Write the goal to a JSON file for persistence
+        File.WriteAllText(userPath, JsonConvert.SerializeObject(this));
+    }
+    public void Load()
+    {
+        // Don't do anything if data files don't exist yet (e.g. first startup)
+        if (!File.Exists(userPath))
+            return;
+
+        // Read the goal from a JSON file for persistence
+        string json = File.ReadAllText(userPath);
+        JsonConvert.PopulateObject(json, this);
+    }
 }
