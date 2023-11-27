49eb7b0ed6a402101e46e070c020aefe8837055a
hellokugelblitz <hellokugelblitz@gmail.com>
2023-11-27 17:03:53 -0500

    Updated login/register routes

--- frontend/nutriapp/src/routes/modals/SignIn.svelte
+++ frontend/nutriapp/src/routes/modals/SignIn.svelte
@@ -1,3 +1,5 @@
+<!-- This is a little test I was doing with hovering modals, I went for a different approach though. -->
+
 <script>
     import { closeModal } from 'svelte-modals'
   
@@ -30,7 +32,7 @@
                   <div class="p-4 md:p-5">
 
                       <!-- "action" here is the api endpoint for auth -->
-                      <form on:submit={closeModal} class="space-y-4" action="api-end-point/">
+                      <form on:submit={closeModal} class="space-y-4" action="/login">
                           <div>
                               <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your username:</label>
                               <input type="username" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-green focus:border-primary-green block w-full p-2.5 " placeholder="username" required>
@@ -41,7 +43,7 @@
                           </div>
                           <button type="submit" class="w-full text-white bg-dark-green mt-6 hover:bg-primary-green focus:ring-4 focus:outline-none focus:ring-light-green font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-all">Login to your account</button>
                           <div class="text-sm font-medium text-gray-500 ">
-                              Not registered? <a href="#" class="text-primary-green hover:underline ">Create account</a>
+                              Not registered? <a href="/" class="text-primary-green hover:underline ">Create account</a>
                           </div>
                       </form>
                   </div>
@@ -68,33 +70,3 @@
     }
 
   </style>
-
-    <!-- 
-    .contents {
-      min-width: 240px;
-      border-radius: 6px;
-      padding: 16px;
-      background: white;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-between;
-      pointer-events: auto;
-    }
-  
-    h2 {
-      text-align: center;
-      font-size: 24px;
-    }
-  
-    p {
-      text-align: center;
-      margin-top: 16px;
-    }
-  
-    .actions {
-      margin-top: 32px;
-      display: flex;
-      justify-content: flex-end;
-    } 
-  
-  </style> -->
