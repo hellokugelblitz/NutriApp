8824d1976a3fba8cd83b2b942e1dc37acb98a973
hellokugelblitz <hellokugelblitz@gmail.com>
2023-11-27 19:15:39 -0500

    Working on login

--- frontend/nutriapp/src/routes/food/+page.svelte
+++ frontend/nutriapp/src/routes/food/+page.svelte
@@ -31,20 +31,20 @@
 
 <div class=" p-4 md:ml-64 mx-0 max-screen max-w-6xl">
     <div class="grid grid-cols-3 gap-4 mx-4">
-    <div class="justify-left col-span-3 row-span-2 p-6 h-max mb-4 border-4 border-gray-225 rounded-full bg-white md:col-span-2">
-            <p class="text-4xl text-gray-800">
-                Food Page
-            </p>
+        <div class="justify-left col-span-3 row-span-2 p-6 h-max mb-4 border-4 border-gray-225 rounded-full bg-white md:col-span-2">
+                <p class="text-4xl text-gray-800">
+                    Food Page
+                </p>
 
-            <div class="mt-14">
-                <button type="button" class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-black bg-white rounded-lg border-2 border-black border-solid  hover:bg-gray-300 active:ring-4 active:outline-none active:ring-primary-green transition ease-in-out">
-                    Sign In
-                </button>
-                <button type="button" class="px-3 mx-4 py-2 text-xs font-medium text-center inline-flex items-center text-black bg-white rounded-lg border-2 border-black border-solid  hover:bg-gray-300 active:ring-4 active:outline-none active:ring-primary-green transition ease-in-out" on:click={() => sendToast()}>
-                    Create an account
-                </button>
+                <div class="mt-14">
+                    <button type="button" class="px-3 py-2 text-xs font-medium text-center inline-flex items-center text-black bg-white rounded-lg border-2 border-black border-solid  hover:bg-gray-300 active:ring-4 active:outline-none active:ring-primary-green transition ease-in-out">
+                        Sign In
+                    </button>
+                    <button type="button" class="px-3 mx-4 py-2 text-xs font-medium text-center inline-flex items-center text-black bg-white rounded-lg border-2 border-black border-solid  hover:bg-gray-300 active:ring-4 active:outline-none active:ring-primary-green transition ease-in-out" on:click={() => sendToast()}>
+                        Create an account
+                    </button>
 
-            </div>
+        </div>
     </div>
     <div class="hidden justify-left col-span-1 p-6 h-48 border-4 border-gray-225 rounded-full bg-white md:flex md:block">
         <p class="text-xl text-gray-800">
--- frontend/nutriapp/src/routes/register/+page.svelte
+++ frontend/nutriapp/src/routes/register/+page.svelte
@@ -1,6 +1,41 @@
-<script>
+<script lang="ts">
     //Some auth stuff happens here
-    import { fade } from 'svelte/transition'
+    import { onMount } from 'svelte';
+
+    let username = '';
+    let password = '';
+
+    const handleSubmit = async (event: SubmitEvent) => {
+    event.preventDefault();
+
+    const credentials = { userName: username, password };
+
+    try {
+        const response = await fetch('http://localhost:5072/api/Auth/signup', {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(credentials),
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            console.log('Registration successful:', data);
+            // redirect here!!!
+        } else {
+            const errorData = await response.json();
+            console.error('Registration failed:', errorData);
+            // handle error
+        }
+        } catch (error) {
+        console.error('Error during registration:', error);
+        }
+    };
+
+  onMount(() => {
+    // You can perform additional setup or actions when the component mounts
+  });
 </script>
 
 
@@ -20,7 +55,7 @@
                     <!-- Modal body -->
                     <div class="p-4 md:p-5">
                         <!-- "action" here is the api endpoint for auth -->
-                        <form class="space-y-4" action="/login">
+                        <form on:submit={handleSubmit} class="space-y-4" action="/login">
                             <div>
                                 <label for="email" class="block mb-2 text-sm font-medium text-gray-900 ">Your username:</label>
                                 <input type="username" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-green focus:border-primary-green block w-full p-2.5 " placeholder="username" required>
