9f4c4643d358b11851648ef9a3866c40cb8fb4da
crazyforMcCafe <rayhmiot@gmail.com>
2023-12-03 16:23:36 -0500

    made App.cs look like main

--- backend/src/main/App.cs
+++ backend/src/main/App.cs
@@ -15,6 +15,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using NutriApp.Controllers.Middleware;
+using NutriApp.Notifications;
+using NutriApp.Save;
+using NutriApp.Teams;
 
 namespace NutriApp;
 
@@ -23,25 +26,29 @@ public class App
     private readonly string userPath = $"{Persistence.UserDataPath}\\user.json";
     private readonly string datePath = $"{Persistence.DateDataPath}\\date.json";
 
+    private ISaveSystem saveSystem;
     private HistoryController history;
     private GoalController goal;
     private WorkoutController workout;
     private FoodController food;
-    private UIController ui;
+    private UserController userCtrl;
+    private TeamController team;
     private DateTime date;
-    private User user;
     private double dayLength;
     private Task<None> timerThread;
-    
+
+    public ISaveSystem SaveSyst => saveSystem;
     public HistoryController HistoryControl => history;
-    public GoalController GoalControl => goal; 
+    public GoalController GoalControl => goal;
     public WorkoutController WorkoutControl => workout;
     public FoodController FoodControl => food;
-    public UIController UIControl => ui;
+    public UserController UserControl => userCtrl;
     public DateTime TimeStamp => date;
-    
-    public User User { get => user; set => user = value; }
-    public double DayLength { set => dayLength = value; }
+
+    public double DayLength
+    {
+        set => dayLength = value;
+    }
 
     public App(double dayLength)
     {
@@ -50,6 +57,8 @@ public class App
         timerThread = new Task<None>(DayLoop);
         timerThread.Start();
 
+        saveSystem = new SaveSystem();
+        userCtrl = new UserController(saveSystem);
         workout = new WorkoutController();
         food = new FoodController(this);
         history = new HistoryController(this, saveSystem);
@@ -70,11 +79,16 @@ public class App
     }
     
 
-    public List<Workout.Workout> GetRecommendedWorkouts() 
-        => workout.GenerateRecommendedWorkouts(history.Workouts);
-    public double GetTodaysCalories() { return -1d; }
+    public List<Workout.Workout> GetRecommendedWorkouts(string username)
+        => workout.GenerateRecommendedWorkouts(history.GetWorkouts(username));
+
+    public double GetTodaysCalories()
+    {
+        return -1d;
+    }
 
     public delegate void DayEventHandler(DateTime date);
+
     public event DayEventHandler DayEndEvent;
 
     public void SubscribeDayEndEvent(DayEventHandler dayEndEvent)
@@ -144,31 +158,6 @@ public class App
             Console.WriteLine("new day " + TimeStamp);
             date = date.AddDays(1d);
         }
-    }
-
-    public void Save()
-    {
-        // Write the user to a JSON file for persistence
-        var userJson = JsonConvert.SerializeObject(user);
-        File.WriteAllText(userPath, userJson);
-        
-        // Write the current date to a JSON file for persistence
-        var timeJson = JsonConvert.SerializeObject(new { date });
-        File.WriteAllText(datePath, timeJson);
-    }
-
-    public void Load()
-    {
-        // Don't do anything if data files don't exist yet (e.g. first startup)
-        if (!File.Exists(userPath) || !File.Exists(datePath))
-            return;
-
-        // Read the user from a JSON file
-        var json = File.ReadAllText(userPath);
-        user = JsonConvert.DeserializeObject<User>(json);
-        
-        // Read the date from a JSON file
-        json = File.ReadAllText(datePath);
-        date = JsonConvert.DeserializeObject<DateTime>(json);
+        return new None();
     }
 }
