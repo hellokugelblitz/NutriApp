d89d36b100da34d2176b6d004d733c4c411833cc
arw5395 <arw5395@rit.edu>
2023-12-04 18:27:14 -0500

    fix undo set weight goal

--- backend/src/undo/undo-command/fitness/UndoSetWeightGoal.cs
+++ backend/src/undo/undo-command/fitness/UndoSetWeightGoal.cs
@@ -5,15 +5,19 @@ class UndoSetWeightGoal : UndoCommand
 {
     private App _app;
     private User _user;
+    private Goal.Goal _prevGoal;
 
-    public UndoSetWeightGoal(App app, User user)
+    public UndoSetWeightGoal(App app, User user, Goal.Goal prevGoal)
     {
         _app = app;
         _user = user;
+        _prevGoal = prevGoal;
     }
 
     public override void Execute()
     {
+        _app.GoalControl.SetGoal(_prevGoal, _user.Name);
+
         onFinished?.Invoke();
     }
 }
