400bd3cc54f735ea02551f6571f1946526496d19
thatnoobles <danildonchuk@gmail.com>
2023-12-05 23:22:30 -0500

    basic basic teams frontend

--- frontend/nutriapp/src/routes/protected/teams/+page.server.ts
+++ frontend/nutriapp/src/routes/protected/teams/+page.server.ts
@@ -1,7 +1,38 @@
+// @ts-nocheck
+
 import type { Actions, PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ locals }) => {
+	// Get team info
+	let team = {
+		name: "",
+		members: [],
+		challengeStartDate: new Date(),
+		challengeEndDate: new Date(),
+	};
+
+	if (!locals.user) return { user: {}, team: team };
+
+	const response = await fetch("http://localhost:5072/api/teams", {
+		method: "GET",
+		headers: { "sessionKey": locals.user.session_key }
+	});
+
+	if (response.ok) {
+		const responseData = await response.json();
+		team = {
+			name: responseData.name,
+			members: responseData.members,
+			challengeStartDate: new Date(responseData.challengeStartDate),
+			challengeEndDate: new Date(responseData.challengeEndDate)
+		}
+	}
+	else {
+		console.log(`${response.status} : ${response.statusText}`)
+	}
+
 	return {
 		user: locals.user,
-	}
+		team: team
+	};
 }
--- frontend/nutriapp/src/routes/protected/teams/+page.svelte
+++ frontend/nutriapp/src/routes/protected/teams/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck lol
+
    import Nav from '$lib/ui/Nav.svelte';
    import { page } from '$app/stores';
    
@@ -10,6 +12,17 @@
  <!-- GRID -->
  <div class=" p-4 md:ml-64 mx-0 max-screen max-w-6xl">
    <div class="flex-col justify-left col-start-1 col-span-3 row-start-1 row-span-3 p-6 max-h-fit mb-4 border-4 border-gray-225 rounded-full bg-white relative">
-      {data.user?.session_key}
+      {data.user.session_key}
+
+      <p class="text-xl font-bold">{data.team["name"]}</p>
+
+      <p class="text-lg mt-4 mb-2">Members</p>
+      {#each data.team.members as member}
+         <p class="text-gray-600">{member}</p>
+      {/each}
+
+      <p class="text-lg mt-6">Current Challenge</p>
+      <p class="text-md mb-4 text-gray-400">{data.team.challengeStartDate.toDateString()} - {data.team.challengeEndDate.toDateString()}</p>
+      <p>(TODO)</p>
    </div>
 </div>
