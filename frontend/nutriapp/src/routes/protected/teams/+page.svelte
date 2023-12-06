f364eb13a6d6a713c66c090f5919d5b19bcd2780
thatnoobles <danildonchuk@gmail.com>
2023-12-05 23:52:21 -0500

    uhhhh h uhmuhm

--- frontend/nutriapp/src/routes/protected/teams/+page.svelte
+++ frontend/nutriapp/src/routes/protected/teams/+page.svelte
@@ -12,17 +12,20 @@
  <!-- GRID -->
  <div class=" p-4 md:ml-64 mx-0 max-screen max-w-6xl">
    <div class="flex-col justify-left col-start-1 col-span-3 row-start-1 row-span-3 p-6 max-h-fit mb-4 border-4 border-gray-225 rounded-full bg-white relative">
-      {data.user.session_key}
-
-      <p class="text-xl font-bold">{data.team["name"]}</p>
-
-      <p class="text-lg mt-4 mb-2">Members</p>
+      <div class="mb-2">
+         <span class="text-xl font-bold">{data.team.name}</span>
+         <span class="text-md text-gray-400">({data.team.members.length} members)</span>
+      </div>
       {#each data.team.members as member}
-         <p class="text-gray-600">{member}</p>
+         <p class="text-lg text-gray-600">{member}</p>
       {/each}
 
-      <p class="text-lg mt-6">Current Challenge</p>
-      <p class="text-md mb-4 text-gray-400">{data.team.challengeStartDate.toDateString()} - {data.team.challengeEndDate.toDateString()}</p>
-      <p>(TODO)</p>
+      <div class="mt-6 mb-2">
+         <span class="text-xl font-bold">Current Challenge</span>
+         <span class="text-md text-gray-400">({data.team.challengeStartDate.toDateString()} - {data.team.challengeEndDate.toDateString()})</span>
+      </div>
+      TODO
    </div>
+
+   <p class="text-gray-400">{data.user.session_key}</p>
 </div>
