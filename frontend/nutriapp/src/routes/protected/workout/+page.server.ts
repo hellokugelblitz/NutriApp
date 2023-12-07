c03776949fd2741d7088eec159dbf131d679cef6
Jack Lindsey-noble (RIT Student) <jgl2651@rit.edu>
2023-12-06 21:58:15 -0500

    Workout page going swell

--- frontend/nutriapp/src/routes/protected/workout/+page.server.ts
+++ frontend/nutriapp/src/routes/protected/workout/+page.server.ts
@@ -1,7 +1,74 @@
+import { redirect } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types"
 
 export const load: PageServerLoad = async ({ locals }) => {
+	
+	try{
+		const response = await fetch('http://localhost:5072/api/Workouts/recommended', {
+			method: 'GET',
+			headers: {
+			'Content-Type': 'application/json',
+			'sessionKey': locals.user?.session_key || ''
+			}
+		});
+
+		if (response.ok) {
+			const data = await response.json();
+			console.log(data);
+			return {
+				user: locals.user,
+				workouts: data
+			}
+		} else {
+			console.log(response);
+		}
+
+	} catch {
+		console.log("Something is going wrong with the workouts");
+	}
+
 	return {
 		user: locals.user,
+		workouts: null
 	}
 }
+
+export const actions = {
+	addworkout: async ({cookies, request, locals}) => {
+		
+        const data = await request.formData();
+        const name = String(data.get('name'));
+        const minutes = Number(String(data.get('minutes')));
+		const intensity = parseFloat(String(data.get('intensity')));
+
+        const body = {
+            name: name,
+            minutes: minutes,
+            intensity: intensity
+        }
+
+
+		//Handling the event that there is no session key hehe
+		let session_key: string = locals.user?.session_key || '';
+		const response = await fetch('http://localhost:5072/api/Workouts', {
+			method: 'POST',
+			headers: {
+			'Content-Type': 'application/json',
+			'sessionKey': session_key
+			},
+			body: JSON.stringify(body)
+		});
+
+		if (response.ok) {
+			//Changes have been made
+			console.log("Added workout: " + name);
+			throw redirect(302, '/protected/workout');
+		} else {
+			//Had issue making changes
+			console.log(response);
+		}
+
+
+        throw redirect(302, '/');
+	}
+} satisfies Actions;
--- frontend/nutriapp/src/routes/protected/workout/+page.svelte
+++ frontend/nutriapp/src/routes/protected/workout/+page.svelte
@@ -3,30 +3,55 @@
    import { page } from '$app/stores';
    
    export let data;
+   const { workouts } = data;
 </script>
 
 <Nav title="Workouts" current_data={$page.data}/>
 
  <!-- GRID -->
-<div class=" p-4 md:ml-64 mx-0 max-screen max-w-6xl">
+<div class=" p-4 md:ml-64 mx-0 max-screen max-w-8xl">
    <!-- TOP ROW -->
    <div class="grid grid-cols-3 gap-5 mb-8 my-4 mx-4">
         <!-- 1 -->
-        <div class="flex p-4 flex-col justify-center h-32 bg-primary-green border-4 border-gray-300 rounded-full font-semibold drop-shadow-sm">
-            <p class="relative top-0 mb-4">Calories Burned:</p>
-            <p class="text-4xl mx-4">100</p>
-        </div>
-
-        <!-- 2 -->
-        <div class="flex p-4 flex-col justify-center h-32 bg-light-green border-4 border-gray-300 rounded-full font-semibold drop-shadow-sm">
-            <p class="relative top-0 mb-4">Current Weight:</p>
-            <p class="text-4xl mx-4">230</p>
+        <div class="relative flex p-4 flex-col col-span-2 justify-center h-96 bg-white border-4 border-gray-300 rounded-full font-semibold drop-shadow-sm w-full">
+            <p class="absolute top-4 mb-8">Previous Workouts:</p>
+            <div class="bg-gray-200 w-full h-3/4 overflow-y-scroll rounded-full">
+                <ul>
+                    {#each workouts as workout, i}
+                      <li class="list-none list-inside" class:even={i % 2 === 0}>
+                        <div class="border-gray-500 border-b-2 w-full h-24 px-8 py-3">
+                          <p>Workout Name: <span class="font-bold">{workout.name}</span></p>
+                          <p>Time Spent: {workout.minutes} minutes</p>
+                          <p>Intensity: {workout.intensity}</p>
+                        </div>
+                      </li>
+                    {/each}
+                  </ul>
+            </div>
         </div>
 
         <!-- 3 -->
-        <div class="flex p-4 flex-col justify-center h-32 bg-nutri-red border-4 border-gray-300 rounded-full font-semibold drop-shadow-sm">                    
-            <p class="relative top-0 mb-4">Goal % Complete:</p>
-            <p class="text-4xl mx-4">27%</p>
+        <div class="flex p-4 flex-col col-start-3 justify-center h-96 w-3/4 bg-white border-4 border-gray-300 rounded-full font-semibold drop-shadow-sm">                    
+            <p class="absolute top-4 mb-8">Create new workout:</p>
+            <form class="space-y-4 w-3/4 absolute top-12" method="POST" action="?/addworkout">
+                <div>
+                    <label for="name" class="block mb-2 text-sm font-medium text-gray-900 ">Workout Name:</label>
+                    <input type="text" name="name" id="name" placeholder="Crunches" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-green focus:border-primary-green block w-full p-2.5 ">
+                </div>
+                <div>
+                    <label for="minutes" class="block mb-2 text-sm font-medium text-gray-900 ">Time Spent:</label>
+                    <input type="number" name="minutes" id="minutes" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-green focus:border-primary-green block w-full p-2.5 " value="">
+                </div>
+                <div>
+                    <label for="intensity" class="block mb-2 text-sm font-medium text-gray-900 ">Intensity:</label>
+                    <select id="intensity" name="intensity" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <option selected value="5">Low</option>
+                        <option value="10">Medium</option>
+                        <option value="15">High</option>
+                      </select>
+                </div>
+                <button type="submit" class="w-full text-white bg-dark-green mt-6 hover:bg-primary-green focus:ring-4 focus:outline-none focus:ring-light-green font-medium rounded-lg text-sm px-5 py-2.5 text-center transition-all">Create Workout</button>
+            </form>
         </div>
     </div>
 </div>
