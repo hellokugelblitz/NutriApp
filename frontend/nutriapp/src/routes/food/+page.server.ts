c19f0e46a8a36306cd73f851549e23de8523fa4f
arw5395 <arw5395@rit.edu>
2023-12-06 19:05:51 -0500

    food page looking good

--- frontend/nutriapp/src/routes/food/+page.server.ts
+++ frontend/nutriapp/src/routes/food/+page.server.ts
@@ -2,7 +2,30 @@ import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
-	return {
-		user: locals.user,
+	try {
+		const [ingredientsResponse, recipesResponse, mealsResponse] = await Promise.all([
+			fetch('http://localhost:5072/api/Ingredients'),
+			fetch('http://localhost:5072/api/Recipes'),
+			fetch('http://localhost:5072/api/Meals')
+		]);
+
+		const ingredients = await ingredientsResponse.json();
+		const recipes = await recipesResponse.json();
+		const meals = await mealsResponse.json();
+
+		return {
+			user: locals.user,
+			ingredients: ingredients.slice(0, 10),
+			recipes: recipes.slice(0, 10),
+			meals: meals.slice(0, 10),
+		};
+	} catch (error) {
+		console.error("Error fetching data:", error);
+		return {
+			user: locals.user,
+			ingredients: [],
+			recipes: [],
+			meals: []
+		};
 	}
 }
--- frontend/nutriapp/src/routes/food/+page.svelte
+++ frontend/nutriapp/src/routes/food/+page.svelte
@@ -1,16 +1,129 @@
 <script>
-    import { page } from '$app/stores';
-    import Nav from '$lib/ui/Nav.svelte';
-    import { SvelteToast, toast } from '@zerodevx/svelte-toast'
+    import { page } from "$app/stores";
+    import Nav from "$lib/ui/Nav.svelte";
+    import { SvelteToast, toast } from "@zerodevx/svelte-toast";
 
-    function sendToast(){
-        toast.push('You can do this action yet :(', { classes: ['custom'] })
+    function sendToast() {
+        toast.push("You can do this action yet :(", { classes: ["custom"] });
     }
 </script>
 
-<Nav title="Food" current_data={$page.data}/>
+<Nav title="Food" current_data={$page.data} />
 
- <!-- GRID -->
-<div class=" p-4 md:ml-64 mx-0 max-screen max-w-6xl">
-    <h1> Food will go here </h1>
+<div class="p-4 md:ml-64 mx-0 max-w-6xl">
+    {#if $page.data.user}
+        <div class="grid md:grid-cols-3 gap-4 text-center">
+            <!-- Ingredients Section -->
+            <div class="p-4 shadow-lg rounded-lg bg-white">
+                <h1 class="text-2xl font-bold mb-4">Ingredients</h1>
+                <ul>
+                    {#each $page.data.ingredients as ingredient}
+                        <li>
+                            <button
+                                class="text-left w-full hover:bg-gray-100 p-2 rounded transition duration-200 ease-in-out"
+                            >
+                                {ingredient.name}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <!-- Recipes Section -->
+            <div class="p-4 shadow-lg rounded-lg bg-white">
+                <h1 class="text-2xl font-bold mb-4">Recipes</h1>
+                <ul>
+                    {#each $page.data.recipes as recipe}
+                        <li>
+                            <button
+                                class="text-left w-full hover:bg-gray-100 p-2 rounded transition duration-200 ease-in-out"
+                            >
+                                {recipe.name}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <!-- Meals Section -->
+            <div class="p-4 shadow-lg rounded-lg bg-white">
+                <h1 class="text-2xl font-bold mb-4">Meals</h1>
+                <ul>
+                    {#each $page.data.meals as meal}
+                        <li>
+                            <button
+                                class="text-left w-full hover:bg-gray-100 p-2 rounded transition duration-200 ease-in-out"
+                            >
+                                {meal.name}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+        </div>
+    {:else}
+        <div class="flex flex-wrap justify-center gap-4 mb-4">
+            <button
+                class="bg-dark-green hover:bg-dark-dark-green text-white py-2 px-4 rounded"
+                >Consume Meal</button
+            >
+            <button
+                class="bg-dark-green hover:bg-dark-dark-green text-white py-2 px-4 rounded"
+                >Create Recipe</button
+            >
+            <button
+                class="bg-dark-green hover:bg-dark-dark-green text-white py-2 px-4 rounded"
+                >Create Meal</button
+            >
+            <button
+                class="bg-dark-green hover:bg-dark-dark-green text-white py-2 px-4 rounded"
+                >View Shopping Cart</button
+            >
+        </div>
+        <div class="grid md:grid-cols-3 gap-4 text-center">
+            <!-- Ingredients Section -->
+            <div class="p-4 shadow-lg rounded-lg bg-white">
+                <h1 class="text-2xl font-bold mb-4">Ingredients</h1>
+                <ul>
+                    {#each $page.data.ingredients as ingredient}
+                        <li>
+                            <button
+                                class="text-left w-full hover:bg-gray-100 p-2 rounded transition duration-200 ease-in-out"
+                            >
+                                {ingredient.name}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <!-- Recipes Section -->
+            <div class="p-4 shadow-lg rounded-lg bg-white">
+                <h1 class="text-2xl font-bold mb-4">Recipes</h1>
+                <ul>
+                    {#each $page.data.recipes as recipe}
+                        <li>
+                            <button
+                                class="text-left w-full hover:bg-gray-100 p-2 rounded transition duration-200 ease-in-out"
+                            >
+                                {recipe.name}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+            <!-- Meals Section -->
+            <div class="p-4 shadow-lg rounded-lg bg-white">
+                <h1 class="text-2xl font-bold mb-4">Meals</h1>
+                <ul>
+                    {#each $page.data.meals as meal}
+                        <li>
+                            <button
+                                class="text-left w-full hover:bg-gray-100 p-2 rounded transition duration-200 ease-in-out"
+                            >
+                                {meal.name}
+                            </button>
+                        </li>
+                    {/each}
+                </ul>
+            </div>
+        </div>
+    {/if}
 </div>
